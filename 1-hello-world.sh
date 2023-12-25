90faaf22ab88354aa4a3b3bbeff067905c05b3b5
Srinivas <gudimella7@gmail.com>
2023-12-24 22:46:26 -0600

    dynamic variable presentation

--- 1-hello-world.sh
+++ 1-hello-world.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# this is comment
 echo "hello shell script"
---
+++ 3-var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=SURESH
+echo "$PERSON1:: hello $PERSON2, good morning"
+echo "$PERSON2:: hi $PERSON1, very gm"
+echo "$PERSON1:: wat's up $PERSON2"
+echo "$PERSON2:: im good wat abt u"
