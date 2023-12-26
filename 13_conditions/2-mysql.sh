84602bf243dd1496ff0839da9c5d35447c4b1f92
Srinivas <gudimella7@gmail.com>
2023-12-25 18:42:29 -0600

    install mysql

--- 13_conditions/2-mysql.sh
+++ 13_conditions/2-mysql.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 if [$ID -ne 0]
 then
-echo "Error ::please run the script with root access"
+     echo "Error ::please run the script with root access"
 else
-echo "you are root user"
+     echo "you are root user"
 fi
